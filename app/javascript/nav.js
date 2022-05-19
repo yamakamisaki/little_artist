5d2f5a3578b8d338ccb262bf0952a74760aa0908
yamakamisaki <s.chuuu02@gmail.com>
2022-05-19 18:35:10 +0900

    ビュー作成（マイページ・プルダウンメニュー）

---
+++ app/javascript/nav.js
@@ -0,0 +1,16 @@
+function pullDown() {
+
+  const mypageBtn = document.getElementById("mypage__btn");
+  const pullDown = document.getElementById("pull-down");
+
+  mypageBtn.addEventListener('click', function() {
+    if (pullDown.getAttribute("style") == "display:block;") {
+        pullDown.removeAttribute("style", "display:block;");
+    } else {
+        pullDown.setAttribute("style", "display:block;");
+    }
+  });
+
+};
+
+window.addEventListener('load', pullDown);

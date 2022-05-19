f8c05fdab304fcc05c2090fbf8ee36c099059c65
yamakamisaki <s.chuuu02@gmail.com>
2022-05-19 22:08:50 +0900

    日本語のエラーメッセージ導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name , presence: true
-
   mount_uploader :image, ImageUploader
 
   has_many :items, dependent: :destroy
   # has_many :comments, dependent: :destroy
   # has_many :favorites, dependent: :destroy
+
+  validates :name , presence: true
 
 end

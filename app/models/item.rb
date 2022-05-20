1060514b9121883813c89f433fdd33547c158819
yamakamisaki <s.chuuu02@gmail.com>
2022-05-20 15:49:32 +0900

    新規投稿・保存機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,15 @@ class Item < ApplicationRecord
   has_one_attached :image
   # has_many :comments, dependent: :destroy
   # has_many :favorites, dependent: :destroy
+
+  with_options presence: true do
+    validates :image
+    validates :age, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 99 ,message: 'は半角数字で入力してください'},
+                      format: { with: /\A[0-9]+\z/ }
+    validates :text, length: { maximum: 200 }
+  end
+
+  validates :material, length: { maximum: 200 }
+  validates :making, length: { maximum: 200 }
+
 end

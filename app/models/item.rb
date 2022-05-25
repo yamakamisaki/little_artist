5bb0b8c2565a5a9bd2cad3ac58b2d96e628e2021
yamakamisaki <s.chuuu02@gmail.com>
2022-05-25 13:15:33 +0900

    お気に入り機能一部実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,7 @@ class Item < ApplicationRecord
   has_one_attached :image
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  # has_many :favorite_users, through: :favorites, source: :user
-
+  
   with_options presence: true do
     validates :image
     validates :age, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 99, message: 'は半角数字で入力してください' },

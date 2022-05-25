00916adef80e025533e772d2cd8fa0ce909fd533
yamakamisaki <s.chuuu02@gmail.com>
2022-05-25 17:52:21 +0900

    j Queryの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,9 @@ require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../nav");
+require("../nav")
+require('jquery')
+require("../comment")
 
 
 

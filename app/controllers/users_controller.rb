117c2b9e37204a17fb458d742e0b83ea1c11b132
yamakamisaki <s.chuuu02@gmail.com>
2022-05-26 22:58:16 +0900

    一覧ページ、詳細ページ編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user,   only: [:show, :favorites]
   before_action :set_search, only: :show
-  before_action :authenticate_user!, only: :favorites
 
   def show
     @search_user = @user.id
@@ -9,7 +8,6 @@ class UsersController < ApplicationController
   end
 
   def favorites
-    redirect_to root_path unless current_user.id 
     @favorites = Favorite.where(user_id: @user.id).all.page(params[:page]).per(9)
   end
 

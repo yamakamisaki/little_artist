9bfcc401850695a28ed3c252e1e20d07db4438d9
yamakamisaki <s.chuuu02@gmail.com>
2022-05-30 17:55:32 +0900

    マイページのレスポンシブ化

--- app/javascript/comment.js
+++ app/javascript/comment.js
@@ -1,11 +1,11 @@
-// // id="comments_areaにをrenderで指定した部分テンプレートに更新
-// $("#comments_area").html("<%= j(render 'comments/item_comment', item: @item, comment: @comment) %>");
-// // フォームを送信したあとにフォームの値を空にする
-// $("textarea").val('');
+// id="comments_areaにをrenderで指定した部分テンプレートに更新
+$("#comments_area").html("<%= (render 'comments/item_comment', item: @item, comment: @comment) %>");
+// フォームを送信したあとにフォームの値を空にする
+$("textarea").val('');
 
-$(function(){
-  $(".comments").css("background-color","pink");
-});
+// $(function(){
+//   $(".comments").css("background-color","pink");
+// });
 
 
 

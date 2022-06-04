d78fcfaf840bdd9c7244148bec0425d7dc0b852c
yamakamisaki <s.chuuu02@gmail.com>
2022-06-04 16:34:06 +0900

    スライドショー実装

--- app/javascript/scroll.js
+++ app/javascript/scroll.js
@@ -1,12 +1,8 @@
-// $(window).on('load', function (){
-//     $(".box7").css("background-color","red").fadeIn("slow");
-//   });
-
 $(function(){
   $('.slider').slick({
     autoplay: true, // 自動でスクロール
     autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
-    speed: 5000, // スライドが流れる速度を設定
+    speed: 7000, // スライドが流れる速度を設定
     cssEase: "linear", // スライドの流れ方を等速に設定
     slidesToShow: 4, // 表示するスライドの数
     swipe: false, // 操作による切り替えはさせない
@@ -17,7 +13,7 @@ $(function(){
       {
         breakpoint: 750,
         settings: {
-          slidesToShow: 3, // 画面幅750px以下でスライド3枚表示
+          slidesToShow: 2, // 画面幅750px以下でスライド3枚表示
         }
       }
     ]

c60b44426bc24e46a613ee50f3366062fbb5b4ed
yamakamisaki <s.chuuu02@gmail.com>
2022-06-04 15:38:21 +0900

    CSS animation追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,9 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../nav")
 require('jquery')
-require('../comment')
-require('../../../app/views/comments/item_comments')
-
+require('../scroll')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)

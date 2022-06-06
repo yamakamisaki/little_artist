eaa16842e7623e9ab9fa6d83d644f39ae74f2f25
yamakamisaki <s.chuuu02@gmail.com>
2022-06-06 16:53:37 +0900

    コメント・ユーザーコントローラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,16 @@ class UsersController < ApplicationController
   before_action :set_search, only: :show
 
   def index
-    @items = Item.all
+    @items = Item.all.order('created_at DESC')
   end
 
   def show
     @search_user = @user.id
-    @items = @user.items.page(params[:page]).per(12)
+    @items = @user.items.order('created_at DESC').page(params[:page]).per(12)
   end
 
   def favorites
-    @favorites = Favorite.where(user_id: @user.id).all.page(params[:page]).per(12)
+    @favorites = Favorite.where(user_id: @user.id).all.order('created_at DESC').page(params[:page]).per(12)
   end
 
   private
